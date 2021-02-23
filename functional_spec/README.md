a726e221bf7b03f73e0e09e784ce8ec66949e144
nitinkurle <kurle.nitin@gmail.com>
2021-02-23 22:58:12 +0530

    Update README.md and index.js file

--- functional_spec/README.md
+++ functional_spec/README.md
@@ -1,1 +1,4 @@
 # parking_lot
+
+Install the dependencies using "npm install".
+To run the application use "node index.js" command.
--- index.js
+++ index.js
@@ -4,10 +4,98 @@ const readLine = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const parkingImplInstance = require('./parkingImpl')
 
 // Get the user input 
 let initMain = () => {
 
+    readLine.on('line', async (input) => {
+        input = input.split(" ");
+        
+        switch (input[0]) {
+            case ('create_parking_lot'):
+
+                try {
+                    const result = await parkingImplInstance.createParkingLot(input[1]);
+                    console.log(result);
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+            case ('park'):
+               // console.log("input", input);
+                try {
+                    const result = await parkingImplInstance.park(input[1].trim(), input[2].trim());
+                    console.log(result);
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+            case ('leave'):
+
+                try {
+                    const result = await parkingImplInstance.leave(input[1]);
+                    console.log(result);
+
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+
+            case ('status'):
+
+                try {
+                    const result = await parkingImplInstance.status();
+                    console.log(result);
+
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+            case ('registration_numbers_for_cars_with_colour'):
+
+                try {
+                    const result = await parkingImplInstance.getRegistrationNumbersFromColor(input[1].trim());
+                    console.log(result);
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+
+            case ('slot_numbers_for_cars_with_colour'):
+
+                try {
+                    const result = await parkingImplInstance.getSlotNumbersFromColor(input[1].trim());
+                    console.log(result);
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+
+            case ('slot_number_for_registration_number'):
+
+                try {
+                    const result = await parkingImplInstance.getSlotNumberFromRegNo(input[1].trim());
+                    console.log(result);
+                } catch (e) {
+                    console.log(`error occured ==> ${e}`);
+                }
+                break;
+
+            case ('exit'):
+                rl.pause();
+                break;
+
+            default:
+                console.log('Seems like an issue with command that you typed , please note predeifed commands are case sensitive and matched as per the description!');
+        }
+    });
 }
 
 initMain();
