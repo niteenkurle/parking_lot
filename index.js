31ec7f6e6c8b65971276f394f6e298d256f41ed1
nitinkurle <kurle.nitin@gmail.com>
2021-02-23 22:36:58 +0530

    Added parking implementation and helpers/utils.js.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const readline = require('readline');
+const readLine = readline.createInterface({    
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Get the user input 
+let initMain = () => {
+
+}
+
+initMain();
